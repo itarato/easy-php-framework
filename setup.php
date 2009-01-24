6fdc50c5107ce94059ac001385731b5ee356226d
U-ITARATODESKTOP\itarato <itarato@itaratodesktop.(none)>
2009-01-24 20:22:13 +0100

    new htaccess strategy, html tidy correction, setup file encode change

--- setup.php
+++ setup.php
@@ -3,14 +3,14 @@
 
 define('DEFAULT_CONTROLLER', "main");
 define('DEFAULT_ACTION', "index");
-define('HTML_PATH', '/easyframework02');
+define('HTML_PATH', '/ef_t2');
 define('DEBUG_MODE', TRUE);
 
 define('DB_TYPE', 'mysql');
 define('DB_HOST', 'localhost');
 define('DB_USER', 'root');
-define('DB_PASS', 'sOmEcOdE');
-define('DB_NAME', 'somedatabasename');
+define('DB_PASS', 'your_code');
+define('DB_NAME', 'your_database_name');
 
 /**
  * Helper function collection helps you to deal with those
@@ -19,5 +19,5 @@ define('DB_NAME', 'somedatabasename');
  * Helpers: ArrayHelper.php, HtmlHelper.php, StringHelper.php
  * e.g.: $helpers = array('Array', 'Html', 'String');
  */
-$helpers = array('Array', 'Html', 'String', 'Constant');
+$helpers = array('Array', 'Html', 'String');
 ?>
